906064dda6ba43c8b10749baf9e978616b54a2a8
Hanna Negash <hnegash.n@gmail.com>
2023-12-12 13:20:48 -0800

    Add labels

--- lib/book.py
+++ lib/book.py
@@ -4,11 +4,13 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-        
+
+    @property
     def page_count(self):
         return self._page_count
     
-    def set_page_count(self, page_count):
+    @page_count.setter
+    def page_count(self, page_count):
         if isinstance(page_count, int):
             self._page_count = page_count
         else:
--- lib/shoe.py
+++ lib/shoe.py
@@ -4,11 +4,13 @@ class Shoe:
     def __init__(self, brand, size):
         self.size = size
         self.brand = brand
-        
+
+    @property
     def size(self):
         return self._size
-     
-    def set_size(self, size):
+    
+    @size.setter
+    def size(self, size):
         if isinstance(size, int):
             self._size = size
         else:
